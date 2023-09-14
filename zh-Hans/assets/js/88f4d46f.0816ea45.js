bba0765d98ffedf8eeaf4271027b5d08f0e6a8a0
jiaqiwang969 <jiaqiwang969@gmail.com>
2023-09-14 21:30:41 +0800

    Deploy website - based on 5fcb9d0cf77b395fe6725e6aa9865085404da000

---
+++ zh-Hans/assets/js/88f4d46f.0816ea45.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksolidworks_gpt=self.webpackChunksolidworks_gpt||[]).push([[67647],{76716:s=>{s.exports=JSON.parse('{"permalink":"/solidworks-GPT/zh-Hans/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
